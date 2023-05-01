4215540119bbebe7668b2671c09378e18b67222c
Stephen <stawuah18@gmail.com>
2023-05-01 00:21:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-const sid = 'ACd0c5201b55c3972808334b6d2a368092'
+const sid = '<Your sid token>'
 
-const auth_token = '0caf73078ca459147ac35b389eb49646'
+const auth_token = '<Your auth_token>'
 
 const twilio = require('twilio')(sid ,auth_token)
 
 twilio.messages.create({
-    from: '+16206589313',
-    to: "+2330201487955",
-    body: "ma last message "
+    from: '<Twilio phone number>',
+    to: "<Enter your your number>",
+    body: "my last message "
 })
 .then((res)=> console.log("Message has  been sent"))
 .catch((e) => console.log('ERROR OCCURED'))
